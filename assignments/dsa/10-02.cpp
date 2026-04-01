0264cd6076234bb8579f1671d097f450674e97db
Yash Kushwaha <yash274602@gmail.com>
2026-04-01 17:16:54 +0530

    add A10-02

--- assignments/dsa/10-02.cpp
+++ assignments/dsa/10-02.cpp
@@ -5,3 +5,22 @@
  Description: In question 1, define a parameterized constructor to initialise member variables.
  Date: 16-02-2026
 */
+#include<iostream>
+using namespace std;
+class Queue
+{
+    private:
+        int capacity,front,rear,*ptr;
+    public:
+        Queue(int c)
+        {
+            capacity=c<=0?1:c;
+            front=rear=-1;
+            ptr=new int[capacity];
+        }
+};
+int main()
+{
+
+    return 0;
+}
