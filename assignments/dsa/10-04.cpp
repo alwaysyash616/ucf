6cd3f7e608ef82341fcf5250d481fcc1aaf0b2a3
Yash Kushwaha <yash274602@gmail.com>
2026-04-02 04:46:06 +0530

    add A10-04

--- assignments/dsa/10-04.cpp
+++ assignments/dsa/10-04.cpp
@@ -5,3 +5,41 @@
  Description: In question 1, define a method to view rear element of the queue.
  Date: 16-02-2026
 */
+#include<iostream>
+using namespace std;
+class Queue
+{
+    private:
+        int capacity,front,rear,*ptr;
+    public:
+        Queue(int c)
+        {
+            capacity=c<=0?1:c;
+            front=rear=-1;
+            ptr=new int[capacity];
+        }
+        void enque(int data)
+        {
+            if((rear+1)%capacity==front)
+            {
+                cout<<"Overflow"<<endl;
+                return;
+            }
+            if(front==-1)
+                front=0;
+            rear=(rear+1)%capacity;
+            ptr[rear]=data;
+        }
+        // View rear element
+        int getBack()
+        {
+            if(front==-1)
+                throw "Queue Underflow";
+            return ptr[rear];
+        }
+};
+int main()
+{
+
+    return 0;
+}
