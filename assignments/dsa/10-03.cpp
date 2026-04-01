6ecac57c4f783462480f1f16adf8e9b8827081c6
Yash Kushwaha <yash274602@gmail.com>
2026-04-01 18:14:57 +0530

    add A10-03

--- assignments/dsa/10-03.cpp
+++ assignments/dsa/10-03.cpp
@@ -5,3 +5,34 @@
  Description: In question 1, define a method to insert a new element at the rear in the queue.
  Date: 16-02-2026
 */
+#include<iostream>
+using namespace std;
+class Queue
+{
+    private:
+        int capacity,front,rear,*ptr;
+    public:
+        Queue(int c)
+        {
+            capacity=c<=0?1:c;
+            front=rear=-1;
+            ptr=new int[capacity];
+        }
+        void enque(int data)
+        {
+            if((rear+1)%capacity==front)
+            {
+                cout<<"Overflow"<<endl;
+                return;
+            }
+            if(front==-1)
+                front=0;
+            rear=(rear+1)%capacity;
+            ptr[rear]=data;
+        }
+};
+int main()
+{
+
+    return 0;
+}
