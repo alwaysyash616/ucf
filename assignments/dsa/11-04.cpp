b019e6f36a5783660c7ffafdde1df1b24971e6a4
Yash Kushwaha <yash274602@gmail.com>
2026-04-03 00:21:32 +0530

    add A11-05

--- assignments/dsa/11-04.cpp
+++ assignments/dsa/11-04.cpp
@@ -5,3 +5,45 @@
  Description: In question 1, define a method to view rear element in the queue.
  Date: 16-02-2026
 */
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+        int item;
+        node *next;
+};
+class Queue
+{
+    private:
+        node *front,*rear;
+    public:
+        Queue()
+        {
+            front=rear=nullptr;
+        }
+        void enque(int data)
+        {
+            node *temp=new node;
+            temp->item=data;
+            temp->next=nullptr;
+            if(rear==nullptr)   // We can check front==nullptr instead.
+                front=rear=temp;
+            else
+            {
+                rear->next=temp;
+                rear=temp;
+            }
+        }
+        int getBack()
+        {
+            if(rear)
+                return rear->item;
+            throw "Queue Underflow";
+        }
+};
+int main()
+{
+
+    return 0;
+}
