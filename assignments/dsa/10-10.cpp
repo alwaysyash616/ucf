9b3539b8665f13e3f339962c876305f723b83094
Yash Kushwaha <yash274602@gmail.com>
2026-04-02 06:02:14 +0530

    add A10-10

--- assignments/dsa/10-10.cpp
+++ assignments/dsa/10-10.cpp
@@ -5,3 +5,88 @@
  Description: In question 1, Define a method to count number of elements present in the queue
  Date: 16-02-2026
 */
+#include<iostream>
+using namespace std;
+class Queue
+{
+    private:
+        int capacity,front,rear,*ptr;
+    public:
+        Queue(int c)
+        {
+            capacity=c<=0?1:c;
+            front=rear=-1;
+            ptr=new int[capacity];
+        }
+        void enque(int data)
+        {
+            if((rear+1)%capacity==front)
+            {
+                cout<<"Overflow"<<endl;
+                return;
+            }
+            if(front==-1)
+                front=0;
+            rear=(rear+1)%capacity;
+            ptr[rear]=data;
+        }
+        // View rear element
+        int getBack()
+        {
+            if(front==-1)
+                throw "Queue Underflow";
+            return ptr[rear];
+        }
+        // View front element
+        int getFront()
+        {
+            if(front==-1)
+                throw "Queue Underflow";
+            return ptr[front];
+        }
+        void dequeue()
+        {
+            if(front==-1)
+            {
+                cout<<"Queue Underflow"<<endl;
+                return;
+            }
+            if(front==rear)
+                front=rear=-1;
+            else
+            front=(front+1)%capacity;
+        }
+        ~Queue()
+        {
+            capacity=0;     // Optional
+            front=rear=-1;  // Optional
+            delete []ptr;
+            ptr=nullptr;    // Optional
+        }
+        bool isFull()
+        {
+            if((rear+1)%capacity==front)
+                return true;
+            return false;
+        }
+        bool isEmpty()
+        {
+            if(front==-1)
+                return true;
+            return false;
+        }
+        int count()
+        {
+            if(isEmpty())
+                return 0;
+            else if(isFull())   // Optional
+                return capacity;
+            else
+                return front<=rear ? rear-front+1 : capacity-(front-rear-1);
+        }
+};
+int main()
+{
+
+    return 0;
+}
