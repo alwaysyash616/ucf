b70930ee0f753a4689023749b63a250f990e94a8
Yash Kushwaha <yash274602@gmail.com>
2026-04-02 23:56:21 +0530

    add A11-02

--- assignments/dsa/11-02.cpp
+++ assignments/dsa/11-02.cpp
@@ -5,3 +5,26 @@
  Description: In question 1, define a constructor to initialise member variable.
  Date: 16-02-2026
 */
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+        int item;
+        node *next;
+};
+class Queue
+{
+    private:
+        node *front,*rear;
+    public:
+        Queue()
+        {
+            front=rear=nullptr;
+        }
+};
+int main()
+{
+
+    return 0;
+}
