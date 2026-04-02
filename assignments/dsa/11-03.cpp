973ab603ecd96432b884c342b91f4f5631d73046
Yash Kushwaha <yash274602@gmail.com>
2026-04-03 00:10:50 +0530

    add A11-03

--- assignments/dsa/11-03.cpp
+++ assignments/dsa/11-03.cpp
@@ -5,3 +5,39 @@
  Description: In question 1, define a method to insert a new element at the rear in the queue.
  Date: 16-02-2026
 */
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+        int item;
+        node *next;
+};
+class Queue
+{
+    private:
+        node *front,*rear;
+    public:
+        Queue()
+        {
+            front=rear=nullptr;
+        }
+        void enque(int data)
+        {
+            node *temp=new node;
+            temp->item=data;
+            temp->next=nullptr;
+            if(rear==nullptr)   // We can check front==nullptr instead.
+                front=rear=temp;
+            else
+            {
+                rear->next=temp;
+                rear=temp;
+            }
+        }
+};
+int main()
+{
+
+    return 0;
+}
