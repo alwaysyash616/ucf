a6ed8b541dc87384947cdab83639508f40c752c1
Yash Kushwaha <yash274602@gmail.com>
2026-04-06 22:58:40 +0530

    add A14-03

--- assignments/dsa/14-03.cpp
+++ assignments/dsa/14-03.cpp
@@ -5,3 +5,31 @@
  Description: In question 1, define a method to check if the tree is empty.
  Date: 20-02-2026
 */
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+        node *left;
+        int item;
+        node *right;
+};
+class BST
+{
+    private:
+        node *root;
+    public:
+        BST()
+        {
+            root=nullptr;
+        }
+        bool isEmpty()
+        {
+            return root ? false : true ;
+        }
+};
+int main()
+{
+
+    return 0;
+}
