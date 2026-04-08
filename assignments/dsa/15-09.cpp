dc0bd14faa09f3fe88be5c03319836b53d380f88
Yash Kushwaha <yash274602@gmail.com>
2026-04-08 09:02:45 +0530

    add A15-09

--- assignments/dsa/15-09.cpp
+++ assignments/dsa/15-09.cpp
@@ -5,3 +5,27 @@
  Description: Write a recursive function to print cubes of first N natural numbers
  Date: 21-02-2026
 */
+#include<iostream>
+using namespace std;
+void printCubes(int);
+int main()
+{
+    int x;
+    cout<<"Enter a number: ";
+    cin>>x;
+    printCubes(x);
+    cout<<endl;
+    return 0;
+}
+void printCubes(int n)
+{
+    if(n<1)
+        throw "Invalid Input";
+    if(n>1)
+    {
+        printCubes(n-1);
+        cout<<" "<<n*n*n;
+    }
+    else
+        cout<<n*n*n;
+}
