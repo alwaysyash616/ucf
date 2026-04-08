bec25cfbdf1654d0abcad1e4292fbbdaa0e37b4e
Yash Kushwaha <yash274602@gmail.com>
2026-04-08 08:58:12 +0530

    add A15-08

--- assignments/dsa/15-08.cpp
+++ assignments/dsa/15-08.cpp
@@ -5,3 +5,27 @@
  Description: Write a recursive function to print squares of first N natural numbers in reverse order
  Date: 21-02-2026
 */
+#include<iostream>
+using namespace std;
+void printSquaresReverse(int);
+int main()
+{
+    int x;
+    cout<<"Enter a number: ";
+    cin>>x;
+    printSquaresReverse(x);
+    cout<<endl;
+    return 0;
+}
+void printSquaresReverse(int n)
+{
+    if(n<1)
+        throw "Invalid Input";
+    if(n>1)
+    {
+        cout<<n*n<<" ";
+        printSquaresReverse(n-1);
+    }
+    else
+        cout<<n*n;
+}
