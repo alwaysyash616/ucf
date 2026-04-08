9f0f30b65b99967a5ff730528cda34c14194a4dc
Yash Kushwaha <yash274602@gmail.com>
2026-04-08 08:19:41 +0530

    add A15-02

--- assignments/dsa/15-02.cpp
+++ assignments/dsa/15-02.cpp
@@ -5,3 +5,27 @@
  Description: Write a recursive function to print first N natural numbers in reverse order
  Date: 21-02-2026
 */
+#include<iostream>
+using namespace std;
+void printNaturalNumbersReverse(int);
+int main()
+{
+    int x;
+    cout<<"Enter a number: ";
+    cin>>x;
+    printNaturalNumbersReverse(x);
+    cout<<endl;
+    return 0;
+}
+void printNaturalNumbersReverse(int n)
+{
+    if(n<1)
+        throw "Invalid Input";
+    if(n>1)
+    {
+        cout<<n<<" ";
+        printNaturalNumbersReverse(n-1);
+    }
+    else
+        cout<<n;
+}
