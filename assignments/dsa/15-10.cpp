bbde1cf26e05045f8295289c637c0a280fd96e93
Yash Kushwaha <yash274602@gmail.com>
2026-04-08 09:04:35 +0530

    add A15-10

--- assignments/dsa/15-10.cpp
+++ assignments/dsa/15-10.cpp
@@ -5,3 +5,27 @@
  Description: Write a recursive function to print cubes of first N natural numbers in reverse order
  Date: 21-02-2026
 */
+#include<iostream>
+using namespace std;
+void printCubesReverse(int);
+int main()
+{
+    int x;
+    cout<<"Enter a number: ";
+    cin>>x;
+    printCubesReverse(x);
+    cout<<endl;
+    return 0;
+}
+void printCubesReverse(int n)
+{
+    if(n<1)
+        throw "Invalid Input";
+    if(n>1)
+    {
+        cout<<n*n*n<<" ";
+        printCubesReverse(n-1);
+    }
+    else
+        cout<<n*n*n;
+}
