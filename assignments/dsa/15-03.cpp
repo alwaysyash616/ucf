58f0c139d3bc0fce35d73173791c347e012436f9
Yash Kushwaha <yash274602@gmail.com>
2026-04-08 08:28:08 +0530

    A15-03

--- assignments/dsa/15-03.cpp
+++ assignments/dsa/15-03.cpp
@@ -5,3 +5,27 @@
  Description: Write a recursive function to print first N odd natural numbers
  Date: 21-02-2026
 */
+#include<iostream>
+using namespace std;
+void printOddNumbers(int);
+int main()
+{
+    int x;
+    cout<<"Enter a number: ";
+    cin>>x;
+    printOddNumbers(x);
+    cout<<endl;
+    return 0;
+}
+void printOddNumbers(int n)
+{
+    if(n<1)
+        throw "Invalid Input";
+    if(n>1)
+    {
+        printOddNumbers(n-1);
+        cout<<" "<<2*n-1;
+    }
+    else
+        cout<<2*n-1;
+}
