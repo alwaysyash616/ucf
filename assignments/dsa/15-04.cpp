db8d36d1e8e6191d2daf23c6018594c14af78f07
Yash Kushwaha <yash274602@gmail.com>
2026-04-08 08:34:45 +0530

    add A15-04

--- assignments/dsa/15-04.cpp
+++ assignments/dsa/15-04.cpp
@@ -5,3 +5,27 @@
  Description: Write a recursive function to print first N odd natural numbers in reverse order
  Date: 21-02-2026
 */
+#include<iostream>
+using namespace std;
+void printOddNumbersReverse(int);
+int main()
+{
+    int x;
+    cout<<"Enter a number: ";
+    cin>>x;
+    printOddNumbersReverse(x);
+    cout<<endl;
+    return 0;
+}
+void printOddNumbersReverse(int n)
+{
+    if(n<1)
+        throw "Invalid Input";
+    if(n>1)
+    {
+        cout<<2*n-1<<" ";
+        printOddNumbersReverse(n-1);
+    }
+    else
+        cout<<2*n-1;
+}
