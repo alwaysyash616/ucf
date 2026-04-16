ba63e3f5b91b798b74825c7a90f0e3e6f31dbee3
Yash Kushwaha <yash274602@gmail.com>
2026-04-16 13:07:18 +0530

    add A17-02

--- assignments/dsa/17-02.cpp
+++ assignments/dsa/17-02.cpp
@@ -13,6 +13,41 @@ class Graph
         int v_count;
         int e_count;
         int **adj;
+    public:
+        Graph(int v,int e)
+        {
+            v_count=v;
+            e_count=e;
+            createGraph();
+        }
+        void createGraph()
+        {
+            int i,j,u,v;
+            adj=new int*[v_count];
+            for(i=0;i<v_count;i++)
+                adj[i]=new int[v_count];
+            for(i=0;i<v_count;i++)
+                for(j=0;j<v_count;j++)
+                    adj[i][j]=0;
+            cout<<"Provide edges"<<endl;
+            for(i=0;i<e_count;)
+            {
+                cin>>u>>v;
+                if(u<0 || v<0 || u>=v_count || v>=v_count)
+                {
+                    cout<<"Invalid Edge"<<endl;
+                    continue;
+                }
+                if(adj[u][v]==1)
+                {
+                    cout<<"Duplicate Edge"<<endl;
+                    continue;
+                }
+                adj[u][v]=1;
+                adj[v][u]=1;
+                i++;
+            }
+        }
 };
 int main()
 {
