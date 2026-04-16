2459ca8e8c5fb0ae8a602d692a92875710dba38b
Yash Kushwaha <yash274602@gmail.com>
2026-04-16 13:42:22 +0530

    add A17-06

--- assignments/dsa/17-06.cpp
+++ assignments/dsa/17-06.cpp
@@ -13,9 +13,96 @@ class Graph
         int v_count;
         int e_count;
         int **adj;
+    public:
+        Graph(int v,int e)
+        {
+            v_count=v;
+            e_count=e;
+            createGraph();
+        }
+        void createGraph()
+        {
+            int i,j,u,v;
+            adj=new int*[v_count];
+            for(i=0;i<v_count;i++)
+                adj[i]=new int[v_count];
+            for(i=0;i<v_count;i++)
+                for(j=0;j<v_count;j++)
+                    adj[i][j]=0;
+            cout<<"Provide edges"<<endl;
+            for(i=0;i<e_count;)
+            {
+                cin>>u>>v;
+                if(u<0 || v<0 || u>=v_count || v>=v_count)
+                {
+                    cout<<"Invalid Edge"<<endl;
+                    continue;
+                }
+                if(adj[u][v]==1)
+                {
+                    cout<<"Duplicate Edge"<<endl;
+                    continue;
+                }
+                adj[u][v]=1;
+                adj[v][u]=1;
+                i++;
+            }
+        }
+        void printGraphMatrix()
+        {
+            int i,j;
+            cout<<"Adjacency Matrix:"<<endl;
+            for(i=0;i<v_count;i++)
+            {
+                for(j=0;j<v_count;j++)
+                    cout<<adj[i][j]<<" ";
+                cout<<endl;
+            }
+        }
+        void printAdjacentNodes(int v)
+        {
+            int i;
+            bool found=false;
+            if(v<0 || v>=v_count)
+            {
+                cout<<"Invalid Node"<<endl;
+                return;
+            }
+            cout<<"Adjacent nodes of v"<<v<<": ";
+            for(i=0;i<v_count;i++)
+            {
+                if(adj[v][i]==1)
+                {
+                    cout<<"v"<<i<<" ";
+                    found=true;
+                }
+            }
+            if(found==false)
+                cout<<"nothing found"<<endl;
+        }
+        bool isIsolated(int v)
+        {
+            int i;
+            if(v<0 || v>=v_count)
+                throw "Invalid Node";
+            for(i=0;i<v_count;i++)
+                if(adj[v][i]==1)
+                    return false;
+            return true;
+        }
+        ~Graph()
+        {
+            int i;
+            for(i=0;i<v_count;i++)
+                delete []adj[i];
+            delete []adj;
+            adj=nullptr;
+        }
 };
 int main()
 {
-
+    // Graph g1(4,3);
+    // g1.printGraphMatrix();
+    cout<<endl;
     return 0;
 }
