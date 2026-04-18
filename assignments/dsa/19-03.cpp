946e5554f7296251814f825c18692a96cac8572f
Yash Kushwaha <yash274602@gmail.com>
2026-04-19 03:05:25 +0530

    add A19-03

--- assignments/dsa/19-03.cpp
+++ assignments/dsa/19-03.cpp
@@ -5,3 +5,33 @@
  Description: Define a function to implement insertion sort.
  Date: 24-02-2026
 */
+#include<iostream>
+using namespace std;
+void insertionSort(int arr[],int n)
+{
+    int i,j,temp;
+    for(i=1;i<n;i++)
+    {
+        temp=arr[i];
+        for(j=i-1;j>=-1;j--)
+        {
+            if(j>=0 && arr[j]>temp)
+                arr[j+1]=arr[j];
+            else
+            {
+                arr[j+1]=temp;
+                break;
+            }
+        }
+    }
+}
+int main()
+{
+    int a[10]={23,100,1,-19,6,81,10,9,11,-1};
+    insertionSort(a,10);
+    cout<<"Sorted array:"<<endl;
+    for(int i=0;i<10;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    return 0;
+}
