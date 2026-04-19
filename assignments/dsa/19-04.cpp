d1ed1dd7bd8ed13378a30409006078f4d1557ac9
Yash Kushwaha <yash274602@gmail.com>
2026-04-19 16:17:21 +0530

    add A19-04

--- assignments/dsa/19-04.cpp
+++ assignments/dsa/19-04.cpp
@@ -5,3 +5,28 @@
  Description: Define a function to implement selection sort.
  Date: 24-02-2026
 */
+#include<iostream>
+using namespace std;
+void selectionSort(int arr[],int n)
+{
+    int i,j,s,temp;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i,s=i;j<n;j++)
+            if(arr[j]<arr[s])
+                s=j;
+        temp=arr[i];
+        arr[i]=arr[s];
+        arr[s]=temp;
+    }
+}
+int main()
+{
+    int a[10]={23,100,1,-19,6,81,10,9,11,-1};
+    selectionSort(a,10);
+    cout<<"Sorted array:"<<endl;
+    for(int i=0;i<10;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    return 0;
+}
