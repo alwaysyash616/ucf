98eb00dce212347a6c4e5b69653f6e0634a9b7f7
Yash Kushwaha <yash274602@gmail.com>
2026-05-15 15:08:05 +0530

    add A09-02

--- assignments/stl/09-02.cpp
+++ assignments/stl/09-02.cpp
@@ -2,6 +2,29 @@
  Assignment: 09
  Problem: 02
  Topic: stack
- Description: 
+ Description: Reverse a stack of strings.
  Date: 04-03-2026
 */
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+void reverseStack(stack <string>&);
+int main()
+{
+    // stack <string> s1;
+    cout<<endl;
+    return 0;
+}
+
+void reverseStack(stack <string> &s)
+{
+    vector <string> temp;
+    while (!s.empty())
+    {
+        temp.push_back(s.top());
+        s.pop();
+    }
+    for(vector<string>::const_iterator cit=temp.begin();cit!=temp.end();cit++)
+        s.push(*cit);
+}
