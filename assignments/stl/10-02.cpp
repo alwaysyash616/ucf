ab53fa3ee60864ae9401bf90c7619d361874b241
Yash Kushwaha <yash274602@gmail.com>
2026-05-21 19:20:27 +0530

    add A10-02

--- assignments/stl/10-02.cpp
+++ assignments/stl/10-02.cpp
@@ -1,7 +1,9 @@
 /*
  Assignment: 10
  Problem: 02
- Topic: Queue
- Description: 
+ Topic: queue
+ Description: Implement priority queue with the given priority range from 1 to N. [Use vector of queues]
  Date: 04-03-2026
 */
+
+//
