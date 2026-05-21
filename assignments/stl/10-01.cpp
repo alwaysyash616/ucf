0ebc3a218c765c13e6f101d0bb7422e10f273393
Yash Kushwaha <yash274602@gmail.com>
2026-05-21 19:04:06 +0530

    add A10-01

--- assignments/stl/10-01.cpp
+++ assignments/stl/10-01.cpp
@@ -1,7 +1,45 @@
 /*
  Assignment: 10
  Problem: 01
- Topic: Queue
- Description: 
+ Topic: queue
+ Description: Implement stack using queue.
  Date: 04-03-2026
 */
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+class Stack
+{
+    private:
+        queue <int> Stack;
+    public:
+        void push(int data)
+        {
+            vector <int> temp;
+            temp.push_back(data);
+            while (!Stack.empty())
+            {
+                temp.push_back(Stack.front());
+                Stack.pop();
+            }
+            for(vector<int>::const_iterator cit=temp.cbegin();cit!=temp.cend();cit++)
+                Stack.push(*cit);                            
+        }
+        void pop()
+        {
+            if(!Stack.empty())
+                Stack.pop();
+        }
+        int top()
+        {
+            if(!Stack.empty())
+                return Stack.front();
+            throw "Stack Underflow";
+        }
+};
+int main()
+{
+
+    return 0;
+}
