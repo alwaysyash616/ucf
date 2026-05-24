3af410cc52c0ac76aa4d865945996077114d9cf6
Yash Kushwaha <yash274602@gmail.com>
2026-05-24 10:56:15 +0530

    add A10-05

--- assignments/stl/10-05.cpp
+++ assignments/stl/10-05.cpp
@@ -1,7 +1,7 @@
 /*
  Assignment: 10
  Problem: 05
- Topic: Queue
- Description: 
+ Topic: queue
+ Description: Given a square chessboard of N x N size, the position of Knight and the position of a target are given. Write a program to find our the minimum steps a Knight will take to reach the target position.
  Date: 04-03-2026
 */
